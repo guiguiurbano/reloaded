d2b255cd85f812b8b3646897dcb847faae0d0353
Guilherme Urbano <guilhermeurbano@MacBook-Pro-de-Guilherme.local>
2024-10-22 15:33:37 +0100

    ola

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -1,16 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   teste.c                                            :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: guilhermeurbano <guilhermeurbano@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 12:39:41 by guilhermeur       #+#    #+#             */
-/*   Updated: 2024/10/22 13:09:06 by guilhermeur      ###   ########.fr       */
+/*   Created: 2024/10/22 15:20:44 by guilhermeur       #+#    #+#             */
+/*   Updated: 2024/10/22 15:30:47 by guilhermeur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+void ft_print_alphabet(void)
+{
+    write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+}
 int main()
 {
+    ft_print_alphabet();
     return 0;
 }
